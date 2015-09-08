f17ffb678bcdafd4c1c433ab822b369c5930f506
michaldabski <contact@michaldabski.com>
2015-09-09 00:12:02 +0100

    Implement linked list

---
+++ src/com/michaldabski/linked_lists/LinkedList.java
@@ -0,0 +1,79 @@
+package com.michaldabski.linked_lists;
+
+import java.util.Objects;
+
+/**
+ * Created by Michal on 08/09/2015.
+ */
+public class LinkedList<T> {
+    static class Node<T> {
+        Node<T> next;
+        final T data;
+
+        Node(T data) {
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[%s]", data.toString());
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Node<?>) {
+                final Node otherNode = (Node) obj;
+                if (Objects.equals(this.data, otherNode.data)) return true;
+            }
+            return false;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        void addNode(Node<T> newNode) {
+            if (next == null) next = newNode;
+            else next.addNode(newNode);
+        }
+    }
+
+    Node<T> head = null;
+
+    public LinkedList(T... values) {
+        if (values == null) addValue(null);
+        else for (T value : values) addValue(value);
+    }
+
+    public Node<T> addValue(T value) {
+        final Node<T> node = new Node<T>(value);
+        if (head == null) head = node;
+        else head.addNode(node);
+        return node;
+    }
+
+    @Override
+    public String toString() {
+        if (head == null) return "";
+        Node node = head;
+        String result = node.toString();
+        while ((node = node.next) != null) {
+            result += "->" + node.toString();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof LinkedList<?>) {
+            LinkedList<?> otherList = (LinkedList<?>) obj;
+            Node<T> node = head;
+            Node<?> otherNode = otherList.head;
+            if (Objects.equals(node, otherNode) == false) return false;
+            while (((node = node.next) != null) | ((otherNode = otherNode.next) != null)) {
+                if (Objects.equals(node, otherNode) == false) return false;
+            }
+            return true;
+        } else return false;
+    }
+}
---
+++ src/com/michaldabski/linked_lists/tests/LinkedListTest.java
@@ -0,0 +1,57 @@
+package com.michaldabski.linked_lists.tests;
+
+
+import com.michaldabski.linked_lists.LinkedList;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Created by Michal on 08/09/2015.
+ */
+public class LinkedListTest {
+
+    @Test
+    public void testConstructor() throws Exception {
+        new LinkedList<String>(null);
+        new LinkedList<String>("test", null);
+        new LinkedList<Integer>(342, null);
+        new LinkedList<Boolean>(true);
+    }
+
+    @Test
+    public void testAddValue() throws Exception {
+        LinkedList<Integer> list = new LinkedList<Integer>(1, 2, 3);
+        list.addValue(4);
+        assertEquals(new LinkedList<Integer>(1, 2, 3, 4), list);
+    }
+
+    @Test
+    public void testAddValueEmpty() throws Exception {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.addValue(4);
+        assertEquals(new LinkedList<Integer>(4), list);
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        LinkedList<Integer> list = new LinkedList<Integer>(1, 2, 3);
+        assertEquals("[1]->[2]->[3]", list.toString());
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+        final LinkedList<Integer> testList = new LinkedList<Integer>(1, 2, 3);
+
+        assertEquals(testList, new LinkedList<Integer>(1, 2, 3));
+
+        assertNotEquals(testList, new LinkedList<Integer>(1, 2, 3, 4));
+        assertNotEquals(testList, new LinkedList<Integer>(2, 3, 4));
+        assertNotEquals(testList, new LinkedList<Integer>(1, 2));
+        assertNotEquals(testList, new LinkedList<Integer>());
+        assertNotEquals(testList, new LinkedList<Integer>(null));
+        assertNotEquals(testList, new LinkedList<Integer>(null, null));
+    }
+}
